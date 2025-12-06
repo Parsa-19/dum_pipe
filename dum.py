d5b5bf50894d3abf6b09c9495f30f13348d8be9d
Parsa-19 <parsakhalife19@gmail.com>
2025-12-06 17:50:48 +0330

    structured with if name == main

--- dum.py
+++ dum.py
@@ -1,2 +1,4 @@
-print("Successfully Piped BITCH 2!! to get that pipe wide open dude..")
-print("lets see if this ubunt..")
+if __name__ == "__main__":
+    print("Successfully Piped BITCH 2!! to get that pipe wide open dude..")
+    print("lets see if this ubunt..")
+print('script done')
